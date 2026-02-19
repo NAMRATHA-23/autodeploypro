d671191d231971cb0f482b1a641224375f2a9199
Namratha Bhandary <namrathabhandary23@gmail.com>
2026-02-19 17:55:39 -0600

    fix: handle env vars in production

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const client = require('prom-client');
-require('dotenv').config();
+// Only load .env file in local development
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 const app = express();
 const PORT = process.env.PORT || 3000;
